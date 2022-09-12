bc5e6e5a545e1c8facd14002c3b54ccce0fba271
ImeldaWidya <113276394+ImeldaWidya@users.noreply.github.com>
2022-09-13 00:30:41 +0800

    Update Tugas1.sh

--- Tugas1.sh
+++ Tugas1.sh
@@ -2,8 +2,8 @@
 printf '%.0s-' {1..35}
 echo -e "\nTugas 1 SISOP Kelas-A"
 printf '%.0s-' {1..35}
-echo -e "\nNama : Aisyah Kirana Putri Isyanto"
-echo "NPM : 21083010065"
+echo -e "\nNama : Imelda Widya Nigrum"
+echo "NPM : 21083010052"
 printf '%.0s-' {1..35}
 echo -e "\nnotes:"
 printf '%.0s-' {1..35}
